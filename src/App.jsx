df358e872b5642eed12a0c1a7f02aa852fb9b628
Maruf <marufalam12ab12@gmail.com>
2026-01-19 19:00:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-bold text-[40px]'>Knowledge Cafe</h1>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default App
